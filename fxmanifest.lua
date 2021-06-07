197af045ecbf38e9daf8c17433cf510ca2ab0ff0
Abel Gaming <lcpdpolice4939@gmail.com>
2021-06-07 12:37:07 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'bodacious'
 game 'gta5'
 description 'ESX Trash Search - Search dumpsters for deposited items!'
 author 'Abel Gaming'
-version '1.0'
+version '1.1'
 
 server_scripts {
 	'config.lua',
